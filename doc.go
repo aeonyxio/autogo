dbff3685d824c9840517c26135512aa734be70f4
aeonyxio <aeonyxio@gmail.com>
2022-06-03 02:55:57 +0100

    doc

---
+++ doc.go
@@ -0,0 +1,19 @@
+package autogo
+
+// blank imports help docs.
+import (
+	// keyboard package
+	_ "github.com/aeonyxio/autogo/keyboard"
+	// mouse package
+	_ "github.com/aeonyxio/autogo/mouse"
+	// point package
+	_ "github.com/aeonyxio/autogo/point"
+	// rectangle package
+	_ "github.com/aeonyxio/autogo/rectangle"
+	// screenshot package
+	_ "github.com/aeonyxio/autogo/screenshot"
+	// system package
+	_ "github.com/aeonyxio/autogo/system"
+	// window package
+	_ "github.com/aeonyxio/autogo/window"
+)
